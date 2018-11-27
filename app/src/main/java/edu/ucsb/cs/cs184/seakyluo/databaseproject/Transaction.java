588fd5da7c3a7337b9f4947f98538bdd990ef8d7
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-11-26 16:52:42 -0800

    Update

--- app/src/main/java/edu/ucsb/cs/cs184/seakyluo/databaseproject/Transaction.java
+++ app/src/main/java/edu/ucsb/cs/cs184/seakyluo/databaseproject/Transaction.java
@@ -4,13 +4,14 @@ import java.sql.Date;
 
 public class Transaction {
     public static final String TRANSACTION = "Transaction", CID = "cid", FROM = "from", TO = "to", TIME = "time", TYPE = "type", AMOUNT = "amount";
-    public static final String CREATE_TABLE = "CREATE TABLE " + TRANSACTION + "(" + CID + "INTEGER, " +
-                                                                                        FROM + " INTEGER, " +
-                                                                                        TO + " INTEGER, " +
+    public static final String CREATE_TABLE = "CREATE TABLE " + TRANSACTION + "(" + CID + "INTEGER NOT NULL, " +
                                                                                         TIME + " DATE, " +
                                                                                         TYPE + " TEXT, " +
                                                                                         AMOUNT + " REAL" +
-                                                                                        "PRIMARY KEY(" + CID + " , " + TIME + "))";
+                                                                                        FROM + " INTEGER, " +
+                                                                                        TO + " INTEGER, " +
+                                                                                        "PRIMARY KEY(" + CID + ", " + TIME + ")" +
+                                                                                        "FOREIGN KEY(" + CID +") REFERENCES "+ Customer.CUSTOMER + ")";
     public static final String DEPOSIT = "deposit", TOP_UP = "top-up", WITHDRAW = "withdraw", PURCHASE = "purchase", TRANSFER = "transfer",
             COLLECT = "collect", PAY_FRIEND = "pay-friend", WIRE = "wire", WRITE_CHECK = "write-check", ACCRUE_INTEREST = "accrue-interest";
     private int cid, from = 0, to = 0;
